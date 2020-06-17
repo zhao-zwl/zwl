8bd16029bad944e72632f32a0192262a71f670d6
赵文龙 <zhaowenlong@yunzongnet.com>
2020-06-17 20:16:46 +0800

    第二部分

--- fed-e-task-01-01/code/IO.js
+++ fed-e-task-01-01/code/IO.js
@@ -1,4 +1,5 @@
 const fp = require('lodash/fp');
+const fs = require('fs')
 class IO {
     static of(x){
         return new IO(function(){
@@ -12,10 +13,41 @@ class IO {
         //把当前的value和传入的fn组合成一个新的函数
         return new IO(fp.flowRight(fn,this._value))
     }
+    join(){
+        return this._value()
+    }
+    flatMap(fn){
+        return this.map(fn).join()
+    }
 }
 
 //调用
-console.log(process);
-let r = IO.of(process).map(p=>p.execPath)
-console.log(r);
-console.log(r._value());
+// console.log(process);
+// let r = IO.of(process).map(p=>p.execPath)
+// console.log(r);
+// console.log(r._value());
+
+
+let readFile = function(filename){
+    return new IO(function(){
+        return fs.readFileSync(filename,'utf-8')
+    })
+}
+let print = function(x){
+    return new IO(function(){
+        console.log(x)
+        return x
+    })
+}
+
+// let cat = fp.flowRight(print,readFile)
+// //IO(IO(x))
+// let r = cat('package.json')._value()._value()
+
+// console.log(r)
+
+
+let r = readFile('package.json')
+        .flatMap(print)
+        .join()
+console.log(r)
---
+++ fed-e-task-01-01/notes/Monad函子.md
@@ -0,0 +1,3 @@
+Monad（单子）
+* Monad 函子是可以变扁的Pointed函子，IO(IO(x))
+* 一个函子如果具有join和of两个方法并遵守一些定律就是一个Monad
